e8a545507266c066033f3a1e375358dd202a7e04
firairo <firairo@gmail.com>
2021-09-29 18:35:08 +0300

    Add animation in view

--- app/src/main/java/by/lexshi/catsapi/MainActivity.kt
+++ app/src/main/java/by/lexshi/catsapi/MainActivity.kt
@@ -1,8 +1,11 @@
 package by.lexshi.catsapi
 
+import android.animation.AnimatorInflater
+import android.animation.AnimatorSet
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.view.View
 import androidx.core.view.isGone
 import androidx.core.view.isVisible
@@ -16,6 +19,9 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var job: Job
+    lateinit var front_anim: AnimatorSet
+    lateinit var back_anim: AnimatorSet
+    var isFront = true
 
     @DelicateCoroutinesApi
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,6 +34,19 @@ class MainActivity : AppCompatActivity() {
             layoutManager = LinearLayoutManager(this@MainActivity)
         }
         updateView()
+
+
+        val scale = applicationContext.resources.displayMetrics.density
+        binding.rvList.cameraDistance = 8000 * scale
+        binding.cardView.cameraDistance = 8000 * scale
+
+        front_anim = AnimatorInflater.loadAnimator(applicationContext, R.animator.front_animator) as AnimatorSet
+        back_anim = AnimatorInflater.loadAnimator(applicationContext, R.animator.back_animator) as AnimatorSet
+
+        //Слушатель нажатия на полную картинку, когда рекуклер isGone
+        binding.fullImageView.setOnClickListener {
+            flip()
+        }
     }
 
 
@@ -47,16 +66,48 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun fullSize(text: String, url: String){
-            binding.rvList.visibility = View.GONE
-            binding.cardView.visibility = View.VISIBLE
-            binding.fullTextView.text = text
+    fun fullSize(text: String, url: String) {
 
-            Glide
-                .with(this@MainActivity)
-                .load(url)
-                .placeholder(R.drawable.ic_launcher_background)
-                .into(binding.fullImageView)
+        /* Скрываем и показываем вьюхи
+        binding.rvList.visibility = View.GONE
+        binding.cardView.visibility = View.VISIBLE*/
+        Glide
+            .with(this@MainActivity)
+            .load(url)
+            .placeholder(R.drawable.ic_launcher_background)
+            .into(binding.fullImageView)
+        binding.fullTextView.text = text
+        flip()
+    }
+
+    private fun flip() {
+        val handler = Handler()
+        if (isFront) {
+            front_anim.setTarget(binding.rvList)
+            back_anim.setTarget(binding.cardView)
+            front_anim.start()
+            back_anim.start()
+            isFront = false
+            binding.cardView.visibility = View.VISIBLE
+
+            handler.postDelayed({
+                binding.rvList.visibility = View.GONE
+            },1000)
+
+
+        } else {
+            front_anim.setTarget(binding.cardView)
+            back_anim.setTarget(binding.rvList)
+            front_anim.start()
+            back_anim.start()
+            binding.cardView.visibility = View.VISIBLE
+            isFront = true
+            binding.rvList.visibility = View.VISIBLE
+            val handler = Handler()
+            handler.postDelayed({
+                binding.cardView.visibility = View.GONE
+            }, 1000)
 
         }
+    }
 }
